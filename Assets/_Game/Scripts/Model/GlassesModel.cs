45cf378ece015c9c2aea65133a40e280ae7dfcc2
MeoMotTay <hieungs1234@gmail.com>
2026-02-01 04:53:53 +0700

    xong glasses i guess

--- Assets/_Game/Scripts/Model/GlassesModel.cs
+++ Assets/_Game/Scripts/Model/GlassesModel.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 namespace GlobalGameJam
 {
     /// <summary>
-    /// Model: Chứa state của kính (đang đeo hay không)
+    /// Model: Chứa state của kính (đang đeo hay không) và màu kính
     /// </summary>
     [CreateAssetMenu(fileName = "GlassesModel", menuName = "GlobalGameJam/Player/Glasses Model")]
     public class GlassesModel : ScriptableObject
     {
         [SerializeField] private bool isWearingGlasses = false;
+        [SerializeField] private GlassColor currentGlassColor = GlassColor.Red;
 
         public bool IsWearingGlasses
         {
             get => isWearingGlasses;
             set => isWearingGlasses = value;
+        }
+
+        public GlassColor CurrentGlassColor
+        {
+            get => currentGlassColor;
+            set => currentGlassColor = value;
         }
 
         /// <summary>
@@ -22,6 +29,17 @@ namespace GlobalGameJam
         public void ResetState()
         {
             isWearingGlasses = false;
+            currentGlassColor = GlassColor.Red;
         }
+    }
+
+    /// <summary>
+    /// Enum cho các màu kính có thể đeo
+    /// </summary>
+    public enum GlassColor
+    {
+        Red,
+        Green,
+        Blue
     }
 }
--- Assets/_Game/Scripts/ViewModel/GlassesViewModel.cs
+++ Assets/_Game/Scripts/ViewModel/GlassesViewModel.cs
@@ -3,7 +3,7 @@ using System;
 namespace GlobalGameJam
 {
     /// <summary>
-    /// ViewModel: Logic xử lý toggle kính, không biết về Unity Animation
+    /// ViewModel: Logic xử lý toggle kính và đổi màu, không biết về Unity Animation
     /// </summary>
     public class GlassesViewModel
     {
@@ -12,8 +12,10 @@ namespace GlobalGameJam
         // Events để View subscribe
         public event Action OnPutOnGlasses;
         public event Action OnPutOutGlasses;
+        public event Action<GlassColor> OnGlassColorChanged;
 
         public bool IsWearingGlasses => model != null && model.IsWearingGlasses;
+        public GlassColor CurrentGlassColor => model != null ? model.CurrentGlassColor : GlassColor.Red;
 
         public GlassesViewModel(GlassesModel model)
         {
@@ -61,6 +63,43 @@ namespace GlobalGameJam
 
             model.IsWearingGlasses = false;
             OnPutOutGlasses?.Invoke();
+        }
+
+        /// <summary>
+        /// Đổi màu kính (chỉ có hiệu lực khi đang đeo kính)
+        /// </summary>
+        public void ChangeGlassColor(GlassColor newColor)
+        {
+            if (model == null) return;
+
+            if (model.CurrentGlassColor != newColor)
+            {
+                model.CurrentGlassColor = newColor;
+                
+                // Chỉ trigger event nếu đang đeo kính
+                if (model.IsWearingGlasses)
+                {
+                    OnGlassColorChanged?.Invoke(newColor);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Cycle through colors: Red -> Green -> Blue -> Red
+        /// </summary>
+        public void CycleGlassColor()
+        {
+            if (model == null) return;
+
+            GlassColor nextColor = model.CurrentGlassColor switch
+            {
+                GlassColor.Red => GlassColor.Green,
+                GlassColor.Green => GlassColor.Blue,
+                GlassColor.Blue => GlassColor.Red,
+                _ => GlassColor.Red
+            };
+
+            ChangeGlassColor(nextColor);
         }
     }
 }
